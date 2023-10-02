141ff8400ad1bfb0a5d0bd0341523b4d71b13332
nataliakif <nataliakif@gmail.com>
2023-10-02 12:35:11 +0300

    add files

---
+++ src/pages/ViewPage.js
@@ -0,0 +1,85 @@
+import React, { useEffect, useState } from "react";
+import { Widget } from "near-social-vm";
+import { useParams } from "react-router-dom";
+import { useQuery } from "../hooks/useQuery";
+import { useHashRouterLegacy } from "../hooks/useHashRouterLegacy";
+import { useBosLoaderStore } from "../stores/bos-loader";
+
+export default function ViewPage(props) {
+  useHashRouterLegacy();
+
+  const { widgetSrc } = useParams();
+  const query = useQuery();
+  const [widgetProps, setWidgetProps] = useState({});
+  const redirectMapStore = useBosLoaderStore();
+
+  const src = widgetSrc || props.overrideSrc || props.widgets.default;
+  const setWidgetSrc = props.setWidgetSrc;
+  const viewSourceWidget = props.widgets.viewSource;
+
+  useEffect(() => {
+    setWidgetProps(Object.fromEntries([...query.entries()]));
+  }, [query]);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setWidgetSrc(
+        src === viewSourceWidget && query.get("src")
+          ? {
+              edit: query.get("src"),
+              view: null,
+            }
+          : {
+              edit: src,
+              view: src,
+            }
+      );
+    }, 1);
+  }, [src, query, setWidgetSrc, viewSourceWidget]);
+
+  function Thing({ path }) {
+    const parts = path.split("/");
+    if (parts[1] === "widget") {
+      return (
+        <Widget
+          key={path}
+          config={{
+            redirectMap: redirectMapStore.redirectMap,
+          }}
+          src={path}
+          props={widgetProps}
+        />
+      );
+    } else {
+      // TODO: every.thing.view could be abstracted out
+      return (
+        <Widget
+          key={path}
+          config={{
+            redirectMap: redirectMapStore.redirectMap,
+          }}
+          src={"every.near/widget/every.thing.view"}
+          props={{ path, ...widgetProps }}
+        />
+      );
+    }
+  }
+
+  return (
+    <div className="">
+      <div className="row">
+        <div
+          className="d-inline-block position-relative overflow-hidden"
+          style={{
+            "--body-top-padding": "12px",
+            "--body-bottom-padding": "68px",
+            paddingTop: "var(--body-top-padding)",
+            paddingBottom: "var(--body-bottom-padding)",
+          }}
+        >
+          <Thing path={src} />
+        </div>
+      </div>
+    </div>
+  );
+}
